35afd65f63d427da76c0d3db4ce3b0fe05b53ae1
Farhad <rume2207080@stud.kuet.ac.bd>
2025-11-15 00:46:40 +0600

    Front page

---
+++ src/main/java/InputGPAcontroller.java
@@ -0,0 +1,2 @@
+public class InputGPAcontroller {
+}
--- src/main/java/com/example/gpacalculator/HelloController.java
+++ src/main/java/com/example/gpacalculator/HelloController.java
@@ -1,14 +1,27 @@
 package com.example.gpacalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void startGPAcalculator(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("InputGPA.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene Scene = new Scene(root);
+        stage.setScene(Scene);
+        stage.setMaximized(true);
+        stage.setTitle("Preview");
     }
 }
+
